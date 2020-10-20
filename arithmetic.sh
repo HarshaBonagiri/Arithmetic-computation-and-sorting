2e2c4757fa349b61095dce043eb6010e6379a50c
HarshaBonagiri <72388172+HarshaBonagiri@users.noreply.github.com>
2020-10-20 13:10:23 +0530

    Update arithmetic.sh
    compute a*b+c

--- arithmetic.sh
+++ arithmetic.sh
@@ -5,5 +5,6 @@ read -p "Enter number a: " a
 read -p "Enter number b: " b
 read -p "Enter number c: " c
 
-Operation1=`expr "scale=2;$a+(($b*$c))" | bc
+Operation1=`expr "scale=2;$a+(($b*$c))" | bc'
+Operation2=`expr "scale=2;(($a*$b))+$c" | bc`
 
